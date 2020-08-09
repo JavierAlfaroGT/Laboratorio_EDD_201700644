0e678a4b7468a996d79c3f658d1fb02fc0d07d20
Marvin <marvin1ronal@gmail.com>
2020-08-09 14:15:34 -0600

    Calificacion Tarea1

--- tarea1/main.c
+++ tarea1/main.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 int main(){
     printf("hello world\njajajaja\nmortal kombat");
-    return 0;   
+    return 0;
 }
 */
 //*/
@@ -13,29 +13,45 @@ int main(){
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include "ListaC.c"
-#include "NodoC.c"
+#include "ListaC.h"
+#include "NodoC.h"
 
-int main(){
-    NodoA* a=newNodoC(10);
-    NodoA* b=newNodoC(3);
-    NodoC* c=newNodoC(25);
+int main() {
+    NodoA* a=newNodoC(1);
+    NodoA* b=newNodoC(2);
+    NodoC* c=newNodoC(3);
+    NodoC* d=newNodoC(4);
+    NodoC* e=newNodoC(5);
+    NodoC* f=newNodoC(6);
+    NodoC* g=newNodoC(7);
+    NodoC* h=newNodoC(8);
+    NodoC* i=newNodoC(9);
     ListaC* l=nuevaLista();
-    add(l,a);
-    add(l,b);
-    add(l,c);
+    add(l, a);
+    add(l, b);
+    add(l, c);
+    add(l, d);
+    add(l, e);
+    add(l, f);
+    add(l, g);
+    add(l, h);
+    add(l, i);
     //imprimir(l);
 
-    buscarIndex(l,2);//metodo implementado por 201700644
-    eliminarIndex(l,1);//metodo implementado por 201700644
+    //buscarIndex(l,2);//metodo implementado por 201700644
+    //eliminarIndex(l,1);//metodo implementado por 201700644
+    eliminarIndex(l,0);
+    eliminarIndex(l,1);
+    eliminarIndex(l,6);
 
+    buscarIndex(l,2);
     imprimir(l);
 
     generar(l);
     Liberar(l);
     free(l);
     return 0;
-    
-    
-    
+
+
+
 }
